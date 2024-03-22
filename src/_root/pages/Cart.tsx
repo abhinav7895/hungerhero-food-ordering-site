25977666531e6ad4ab107ca6e81a225b31b4d28f
Abhinav Yadav <abhinavay2003@gmail.com>
2024-03-22 18:11:55 +0530

    Add empty cart message and link to home page

--- src/_root/pages/Cart.tsx
+++ src/_root/pages/Cart.tsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom"
+
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <section className="flex container mx-auto mt-20 justify-center items-center flex-col">
+      <img  src={'assets/emptycart.avif'} className="max-w-[430px] w-full" />
+      <h1 className="text-xl mt-4 font-semibold text-gray-700">Your cart is empty</h1>
+      <p className="text-base font-light text-gray-500">You can go to home page to view more restaurants</p>
+      <Link to="/" className="bg-[#d74112] hover:bg-orange-700 transition-all delay-100 mt-4 text-white p-2 px-4 rounded-lg">
+        <button>SEE RESTAURANTS NEAR YOU</button>
+      </Link>
+    </section> 
   )
 }
 
